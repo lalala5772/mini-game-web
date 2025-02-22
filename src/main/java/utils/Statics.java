8c7ad81c5d176aeb1a913fce4267cbd74573fd08
MiR Lee <mirlee@MiRui-MacBookAir.local>
2025-02-22 20:33:33 +0900

    test

--- src/main/java/utils/Statics.java
+++ src/main/java/utils/Statics.java
@@ -2,10 +2,6 @@ package utils;
 
 public class Statics {
 
-		//현재 페이지의 게시판에서 몇개의 게시물이 보일지
-		public static int recordCountPerPage = 5;
-		
-		//현재 페이지에 노출 될 네이비게이터 개수는 몇개로 할 것 인가.
-		public static int naviCountPerPage = 5;
+       
 
 }
